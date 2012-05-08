849050b191b4109912a25168e101f764a4ac55dd
Sander Latour <latour@uva.nl>
2012-05-08 16:39:45 +0200

    Now returns the first 20 videos in json

--- api/videos.php
+++ api/videos.php
@@ -6,10 +6,19 @@
 // Connect to the database
 include("connect.php");
 
+ob_start();
 // Set queries
 $q_get_20_videos = "SELECT * FROM `Videos` LIMIT 20";
 
 $result = mysql_query($q_get_20_videos);
 
+$videos = array();
+
+while( $row = mysql_fetch_assoc($result) ){
+  $videos[] = $row;
+}
+
+ob_end_clean();
+echo json_encode(array("videos"=>$videos));
 
 ?>
