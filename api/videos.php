750379260e0a9ddc1f25a2b3e171472a47d3112d
Sander Latour <latour@uva.nl>
2012-05-27 18:04:32 +0200

    Improved related videos, now specifies score, related keywords, related categories, related terms.

--- api/videos.php
+++ api/videos.php
@@ -14,8 +14,8 @@ $q_get_categories = "SELECT parent, `category_id`, weight AS 'selected_video_wei
 $q_get_related_videos = "SELECT `video_id` AS 'related_video', category_id AS 'related_category', weight AS 'related_video_weight' FROM `Video-Categories` WHERE `parent` = '%s' AND NOT `video_id` = '%s'";
 $q_get_terms = "SELECT term_id FROM `Video-Terms` WHERE video_id =  '%s'";
 $q_get_related_videos_terms = "SELECT `video_id` AS related_video FROM `Video-Terms` WHERE `term_id` = '%s' AND NOT `video_id` = '%s'";
-$q_get_keywords = "SELECT `Keywords` FROM `Video-Keywords` WHERE `video_id` = '%s' ";
-$q_get_related_videos_keywords = "SELECT `video_id` AS related_video FROM `Video-Keywords` WHERE `Keywords` = '%s' AND NOT `video_id` = '%s' AND NOT `Keywords` = '' ";
+$q_get_keywords = "SELECT `keyword` FROM `Video-Keywords` WHERE `video_id` = '%s' ";
+$q_get_related_videos_keywords = "SELECT `video_id` AS related_video FROM `Video-Keywords` WHERE `keyword` = '%s' AND NOT `video_id` = '%s' AND NOT `keyword` = '' ";
 
 if(!isset($_GET['mode'])) $_GET['mode'] = 'list';
 
@@ -23,172 +23,160 @@ switch($_GET['mode']){
   case "related":
     if(isset($_GET['id'])){
       $id = $_GET['id'];
+
+      // Final result structure;
+      $result = array();
+
       ob_start();
+      
+      /*********************************
+       * Relevant videos by categories *
+       *********************************/
+
+      // $w is a constant that is used to make the weight of the selected video
+      // more important than the weight of the related videos.
+      $w = 2;
+
       // Gets categories of selected video.
       $res_cat = mysql_query(
         sprintf(
           $q_get_categories,
           mysql_real_escape_string($id)
-        )
-      );
+          )
+        );
       $categories = array();
       while($row_cat = mysql_fetch_array($res_cat)){
-		$categories[$row_cat['category_id']] = array($row_cat['selected_video_weight'], $row_cat['parent']);
-      }
-
-      //Gets related videos that have matching categories.
-      $related_videos = array();
-      foreach ($categories as $category => $value)
-      {
-		$parent = $value[1];
-        $result_related = mysql_query(
+        $result_related_categories = mysql_query(
           sprintf(
             $q_get_related_videos,
-            mysql_real_escape_string($parent),
+            mysql_real_escape_string($row_cat['parent']),
             mysql_real_escape_string($id)
           )
         );
-
-        while($row_related = mysql_fetch_array($result_related))
+        
+        while($row_related_by_category = mysql_fetch_array($result_related_categories))
         {
-			$related_videos[$row_related['related_video']][$row_related['related_category']] = $row_related['related_video_weight'];
+          // Add related video and its categories to the end result
+          if(
+            ! array_key_exists($row_related_by_category['related_video'], $result) ||
+            ! array_key_exists("categories", $result[$row_related_by_category['related_video']])
+          ){
+            $result[$row_related_by_category['related_video']]["categories"] = array($row_related_by_category['related_category']);
+          }else{
+            $result[$row_related_by_category['related_video']]["categories"][] = $row_related_by_category['related_category'];
+          }
+          // Update score of the related video
+          if( ! array_key_exists("score", $result[$row_related_by_category['related_video']]) ){
+            $result[$row_related_by_category["related_video"]]["score"] = $row_related_by_category['related_video_weight'];
+          }else{
+            $result[$row_related_by_category["related_video"]]["score"] += $row_related_by_category['related_video_weight'];
+          }
+          // Add bonus when leafs match, else add 0.5
+          if( $row_related_by_category['related_category'] == $row_cat['category_id']){
+            $result[$row_related_by_category["related_video"]]["score"] += $w * $row_cat['selected_video_weight'];
+          }else{
+            $result[$row_related_by_category["related_video"]]["score"] += 0.5;
+          }
         }
       }
 
-	$categories_count = array();
-	//$w is a constant that is used to make the weight of the selected video more important than the weight of the related videos.
-	$w = 2;
+      /****************************
+       * Relevant videos by terms *
+       ****************************/
 
-	foreach($related_videos as $related_video => $value)
-	{
-		$score = 0;
-		foreach($value as $category => $weight)
-		{
-			if(array_key_exists($category, $categories))
-			{
-			$score += ($w * $categories[$category][0]) + $weight;
-			}
-			else
-			{
-			$score += 0.5;
-			}
-		}
-		$categories_count[$related_video] = $score;
-	}
-
-     // Gets general terms of selected video.
+      // Gets general terms of selected video.
       $res_term = mysql_query(
         sprintf(
           $q_get_terms,
           mysql_real_escape_string($id)
         )
       );
-      $terms = array();
+      $term_weight = 1;
       while($row_term = mysql_fetch_array($res_term)){
-		$terms[] = $row_term['term_id'];
-      }
-
-	$related_terms = array();
-	foreach ($terms as $term)
-	{
         $result_related_terms = mysql_query(
           sprintf(
             $q_get_related_videos_terms,
-            mysql_real_escape_string($term),
+            mysql_real_escape_string($row_term['term_id']),
             mysql_real_escape_string($id)
           )
         );
 
-		while($row_terms = mysql_fetch_array($result_related_terms))
-		{
-			if(!array_key_exists($row_terms['related_video'], $related_terms))
-			{
-			$related_terms[$row_terms['related_video'] ] = array($term);
-			}
-			else
-			{
-			array_push($related_terms[$row_terms['related_video'] ], $term);
-			}
+        while($row_related_by_term = mysql_fetch_array($result_related_terms))
+        {
+          // Add related video and its keywords to the end result
+          if(
+            ! array_key_exists($row_related_by_term['related_video'], $result) ||
+            ! array_key_exists("terms", $result[$row_related_by_term['related_video']])
+          ){
+            $result[$row_related_by_term['related_video']]["terms"] = array($row_term['term_id']);
+          }else{
+            $result[$row_related_by_term['related_video']]["terms"][] = $row_term['term_id'];
+          }
+          // Update score of the related video
+          if( ! array_key_exists("score", $result[$row_related_by_term['related_video']]) ){
+            $result[$row_related_by_term["related_video"]]["score"] = $term_weight;
+          }else{
+            $result[$row_related_by_term["related_video"]]["score"] += $term_weight;
+          }
 
-		}
+        }
+      }
 
-	}
-
-	$term_count = array();
-	$term_weight = 1;
-	foreach($related_term as $related_video => $value)
-	{
-		$term_count[$related_video] = count($value) * $term_weight;
-	}
-
-     // Gets keywords of selected video.
+      /*******************************
+       * Relevant videos by keywords *
+       *******************************/
+      
+      // Gets keywords of selected video.
       $res_keywords = mysql_query(
         sprintf(
           $q_get_keywords,
           mysql_real_escape_string($id)
         )
       );
-      $keywords = array();
-      while($row_keywords = mysql_fetch_array($res_keyword)){
-		$keywords[] = $row_keywords['Keywords'];
-      }
 
-	$related_keywords = array();
-	foreach ($keywords as $keyword)
-	{
+      $keyword_weight = 8;
+
+      // For each keyword of the current video, retrieve relevant videos.
+      // I.e.: with the same keyword
+      while($row_keywords = mysql_fetch_array($res_keywords)){
         $result_related_keyword = mysql_query(
           sprintf(
             $q_get_related_videos_keywords,
-            mysql_real_escape_string($keyword),
+            mysql_real_escape_string($row_keywords['keyword']),
             mysql_real_escape_string($id)
           )
         );
+        while($row_related_by_keyword = mysql_fetch_array($result_related_keyword)){
+          // Add related video and its keywords to the end result
+          if(
+            ! array_key_exists($row_related_by_keyword['related_video'], $result) ||
+            ! array_key_exists("keywords", $result[$row_related_by_keyword['related_video']])
+          ){
+            $result[$row_related_by_keyword['related_video']]["keywords"] = array(trim($row_keywords['keyword']));
+          }else{
+            $result[$row_related_by_keyword['related_video']]["keywords"][] = trim($row_keywords['keyword']);
+          }
+          // Update score of the related video
+          if( ! array_key_exists("score", $result[$row_related_by_keyword['related_video']]) ){
+            $result[$row_related_by_keyword["related_video"]]["score"] = $keyword_weight;
+          }else{
+            $result[$row_related_by_keyword["related_video"]]["score"] += $keyword_weight;
+          }
+        }
+      }
 
-		while($row_relatedkeyword = mysql_fetch_array($result_related_keyword))
-		{
-			if(!array_key_exists($row_relatedkeyword['related_video'], $related_keywords))
-			{
-			$related_keywords[$row_relatedkeyword['related_video'] ] = array($keyword);
-			}
-			else
-			{
-			array_push($related_keywords[$row_relatedkeyword['related_video'] ],  $keyword);
-			}
-		}
+      //The array with scores is sorted in reverse order, so the related video with the highest score (and thus the biggest match with the selected video) ends at the top.
+//      arsort($endscore);
 
-	}
-
-	$keyword_count = array();
-	$keyword_weight = 8;
-	foreach($related_keywords as $related_video => $value)
-	{
-		$keyword_count[$related_video] = count($value) * $keyword_weight;
-	}
-
-	//ENDSCORE
-	//$endscore contains the final score. The first loop adds the term-score with the category-score...
-	$endscore = $categories_count;
-	foreach($term_count as $key => $value)
-		{
-			$endscore[$key] += $term_count[$key];
-		}
-	//...while the second loop adds the keyword-score.
-	foreach($keyword_count as $key => $value)
-		{
-			$endscore[$key] += $keyword_count[$key];
-		}
-
-	//The array with scores is sorted in reverse order, so the related video with the highest score (and thus the biggest match with the selected video) ends at the top.
-	arsort($endscore);
-      
       ob_end_clean();
-      print_r($categories);
-      echo json_encode($endscore);
+      //print_r($categories);
+      //echo json_encode($endscore);
+      echo json_encode($result);
     }
     break;
   case "details":
     if(isset($_GET['id'])){
-        // popup ...
+      // popup ...
     }else{
 
     }
@@ -208,11 +196,11 @@ switch($_GET['mode']){
 
     if(isset($_GET['scope']) && intval($_GET['scope']) > 0){
       $result = mysql_query(
-        sprintf(
-          $q_get_time_categories_scope,
-          mysql_real_escape_string($_GET['scope'])
-        )
-      );
+          sprintf(
+            $q_get_time_categories_scope,
+            mysql_real_escape_string($_GET['scope'])
+            )
+          );
     }else{    
       $result = mysql_query($q_get_time_categories);
     }
@@ -224,22 +212,22 @@ switch($_GET['mode']){
     if($_GET['limited'] == 1){
       if(isset($_GET['scope']) && intval($_GET['scope']) > 0){
         $result = mysql_query(
-          sprintf(
-            $q_get_videos_scope_limited,
-            mysql_real_escape_string($_GET['scope'])
-          )
-        );
+            sprintf(
+              $q_get_videos_scope_limited,
+              mysql_real_escape_string($_GET['scope'])
+              )
+            );
       }else{
         $result = mysql_query($q_get_videos_limited);
       }
     }else{
       if(isset($_GET['scope']) && intval($_GET['scope']) > 0){
         $result = mysql_query(
-          sprintf(
-            $q_get_videos_scope,
-            mysql_real_escape_string($_GET['scope'])
-          )
-        );
+            sprintf(
+              $q_get_videos_scope,
+              mysql_real_escape_string($_GET['scope'])
+              )
+            );
       }else{
         $result = mysql_query($q_get_videos);
       }
