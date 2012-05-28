090ba54fb7aeee4750bdd26ede6222b678a3cb84
Judith Borghouts <judith_borghouts@hotmail.com>
2012-05-28 22:03:23 +0200

    Correct username + password

--- detail/config.php
+++ detail/config.php
@@ -2,8 +2,8 @@
 // Connection's Parameters, set your sql settings here
 $db_host="localhost";
 $db_name="ACM-SigMM";
-$username="root";
-$password="evpZMNPcAnSdYed3";
+$username="ACM";
+$password="video";
 $db_con=mysql_connect($db_host,$username,$password);
 $connection_string=mysql_select_db($db_name);
 // Connection use these to connect in the php of the actual page you want to access db's with
